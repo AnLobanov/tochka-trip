210c75a2678e7fe478d96aa69acb00574fb9968a
Anton Lobanov <antnlobanov@gmail.com>
2024-06-27 06:55:17 +0500

    try to set alembic from env

--- data/migrations/env.py
+++ data/migrations/env.py
@@ -10,7 +10,7 @@ from os import environ
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("DB_SERVER", environ["DB_SERVER"])
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
