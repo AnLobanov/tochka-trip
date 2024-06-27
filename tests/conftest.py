dd0f6f54115ebf59dc2017f2c1d2d58b4d6b7f34
Anton Lobanov <antnlobanov@gmail.com>
2024-06-27 07:35:12 +0500

    changed mock info

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,18 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.orm import Session
 from sqlalchemy_utils import database_exists, drop_database, create_database
+from datetime import datetime
 from data import database, models
 from main import app
-from alembic.config import Config
-from alembic import command
-from alembic.util.exc import CommandError
 
 @pytest.fixture(scope="session")
 def db_engine():
     engine = create_engine("sqlite:///test.sqlite")
-    # base = declarative_base()
     if not database_exists("sqlite:///test.sqlite"):
         create_database("sqlite:///test.sqlite")
     database.base.metadata.create_all(bind=engine)
-    # try:
-    #     alembic_cfg = Config("alembic.ini")
-    #     command.revision(alembic_cfg, autogenerate=True, message="init")
-    #     command.upgrade(alembic_cfg, "head")
-    # except CommandError as e:
-    #     print(f"Error running Alembic commands: {str(e)}")
     yield engine
     drop_database("sqlite:///test.sqlite")
 
@@ -43,7 +34,7 @@ def db(db_engine):
     db.add(models.Place(plane=2, row=1, place=3))
     db.add(models.Place(plane=2, row=1, place=3))
     db.commit()
-    db.add(models.Flight(number="SU123", arrival="SVO", depatrure="LED", price=1000.99, plane=1, arrivalTime="2021-09-01 12:00:00", depatrureTime="2021-09-01 10:00:00"))
+    db.add(models.Flight(number="SU123", arrival="SVO", depatrure="LED", price=1000.99, plane=1, arrivalTime=datetime(2024, 6, 27, 12, 0, 0, 0), depatrureTime=datetime(2024, 6, 27, 10, 0, 0, 0)))
     db.commit()
     yield db
     db.rollback()
